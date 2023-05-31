482a1cb0327b2c1f8fb87481c4d2a61b88f5748e
dmacsandwich <120608633+dmacsandwich@users.noreply.github.com>
2023-05-30 21:25:21 -0400

    Updated dockerfile to include connection for postgres.

---
+++ app/app/tests.py
@@ -0,0 +1,20 @@
+"""
+Sample tests
+"""
+
+from django.test import SimpleTestCase
+from app import calc
+class CaclTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        """Test adding numberes together"""
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_subtract_numbers(self):
+        """Test subtracting number"""
+        res = calc.subtract(10,15)
+        self.assertEqual(res,5)
